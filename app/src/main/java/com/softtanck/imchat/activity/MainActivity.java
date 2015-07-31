c31aadd586575aa1b4527e30d4ce17d2371079e0
tangce <i0555@cienet.com.cn>
2015-07-31 12:35:06 +0800

    add img.

--- app/src/main/java/com/softtanck/imchat/activity/MainActivity.java
+++ app/src/main/java/com/softtanck/imchat/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.softtanck.imchat.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
@@ -18,99 +20,99 @@ import io.rong.imlib.NativeObject;
 import io.rong.imlib.RongIMClient;
 import io.rong.imlib.model.Conversation;
 import io.rong.imlib.model.Message;
+import io.rong.message.ImageMessage;
 import io.rong.message.TextMessage;
 
 import static io.rong.imlib.model.Conversation.ConversationType;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RongIMClient.OnReceiveMessageListener {
 
     private ListView listView;
 
     private EditText editText;
 
     private ChatAdapter adapter;
-    private List<Message> messages;
+    private List<Message> messages = new ArrayList<>();
     private Message message;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            super.handleMessage(msg);
+            adapter.notifyDataSetChanged();
+            listView.setSelection(messages.size() - 1);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView = (ListView) findViewById(R.id.lv);
         editText = (EditText) findViewById(R.id.et);
 
         Log.d("Tanck", "start");
         String Token = "l+b9+ZKBjf5tlpOn2G2RE+5BZXJeeKSqHSs6ZG1t8pRjFz23cyW53obPJOcToEhzggUnJS+7aaNEzFxyP3z6cg==";//test
-        /**
-         * IMKit SDK调用第二步
-         *
-         * 建立与服务器的连接
-         *
-         */
         RongIMClient.connect(Token, new RongIMClient.ConnectCallback() {
             @Override
             public void onTokenIncorrect() {
-                //Connect Token 失效的状态处理，需要重新获取 Token
-                Log.d("Tanck", "Token失效");
+
             }
 
             @Override
-            public void onSuccess(String userId) {
-                Log.d("Tanck", "userId:" + userId);
-                messages = RongIMClient.getInstance().getHistoryMessages(ConversationType.PRIVATE, "2", -1, 20);
-                if (null == messages) {
-                    messages = new ArrayList<>();
-                }
-                adapter = new ChatAdapter(MainActivity.this, messages);
-                listView.setAdapter(adapter);
+            public void onSuccess(String s) {
+                Log.d("Tanck", "链接成功,当前UserId:" + s);
+                getHistoryMessage();
             }
 
             @Override
             public void onError(RongIMClient.ErrorCode errorCode) {
-                Log.d("Tanck", "error:" + errorCode.getMessage());
+
             }
         });
-        Log.d("Tanck", "end");
-        listView = (ListView) findViewById(R.id.lv);
-
-
-        RongIMClient.getInstance().setOnReceiveMessageListener(new RongIMClient.OnReceiveMessageListener() {
-            @Override
-            public boolean onReceived(Message message, int i) {
-                Log.d("Tanck", "接受到了消息:" + ((TextMessage) message.getContent()).getContent());
-                messages.add(message);
-                updateMsg();
-                return true;
-            }
-        });
+        RongIMClient.setOnReceiveMessageListener(this);
+        adapter = new ChatAdapter(MainActivity.this, messages);
+        listView.setAdapter(adapter);
     }
 
-    public void send(View view) {
-        TextMessage textMessage = TextMessage.obtain(editText.getText().toString());
+    private void getHistoryMessage() {
+        List<Message> tmessages = RongIMClient.getInstance().getHistoryMessages(ConversationType.PRIVATE, "2", -1, 10);
+        Log.d("Tanck", "拿到消息了" + tmessages);
+        messages.clear();
+        messages.addAll(tmessages);
+        adapter.notifyDataSetChanged();
+        listView.setSelection(messages.size() - 1);
+    }
 
-        message = RongIMClient.getInstance().sendMessage(ConversationType.PRIVATE, "2", textMessage, null, null, new RongIMClient.SendMessageCallback() {
+
+    public void send(View view) {
+        message = RongIMClient.getInstance().sendMessage(ConversationType.PRIVATE, "2", TextMessage.obtain(editText.getText().toString()), null, null, new RongIMClient.SendMessageCallback() {
             @Override
             public void onError(Integer integer, RongIMClient.ErrorCode errorCode) {
-                Log.d("Tanck", "发送失败:" + errorCode.getMessage());
                 message.setSentStatus(Message.SentStatus.FAILED);
-                updateMsg();
+                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onSuccess(Integer integer) {
-                Log.d("Tanck", "发送成功:");
+                Log.d("Tanck", "success");
                 message.setSentStatus(Message.SentStatus.SENT);
-                updateMsg();
+                adapter.notifyDataSetChanged();
             }
         });
         messages.add(message);
         editText.setText("");
-        updateMsg();
-    }
-
-    private void updateMsg() {
         adapter.notifyDataSetChanged();
         listView.setSelection(messages.size() - 1);
+    }
+
+    @Override
+    public boolean onReceived(Message message, int i) {
+        Log.d("Tanck", "收到了消息:" + ((TextMessage) message.getContent()).getContent());
+        messages.add(message);
+        handler.sendEmptyMessage(0);
+        return true;
     }
 }
