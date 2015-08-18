3623ab78b9cbd6bae15db0ccab35ffc4baf4b171
tangce <i0555@cienet.com.cn>
2015-08-18 10:47:33 +0800

    update lib,

--- app/src/main/java/com/softtanck/imchat/activity/MainActivity.java
+++ app/src/main/java/com/softtanck/imchat/activity/MainActivity.java
@@ -194,7 +194,6 @@ public class MainActivity extends AppCompatActivity implements RongIMClient.OnRe
 
 
     private void SendMsg(MessageContent messageContent) {
-
         RongIMClient.getInstance().sendMessage(ConversationType.PRIVATE, "2", messageContent, null, null, new RongIMClient.SendMessageCallback() {
             @Override
             public void onError(Integer integer, RongIMClient.ErrorCode errorCode) {
